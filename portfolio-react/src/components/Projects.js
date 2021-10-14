d4efe666274517a71ac0cb376813db90784646f1
Lloyd Menguito <lloydmenguito@gmail.com>
2021-10-14 17:46:21 +0800

    static links

--- portfolio-react/src/components/Projects.js
+++ portfolio-react/src/components/Projects.js
@@ -10,7 +10,7 @@ const Projects = () => {
         <div className="container-fluid bg-light py-2">
             <div className = 'row'>
                 <Frame title = 'Portfolio' src = 'https://marclloyd01.github.io/index.html'/>
-                <Frame title = 'Calendar JavaScript' src = 'calendar.html'/>
+                <Frame title = 'Calendar JavaScript' src = 'https://marclloyd01.github.io/src/Projects/calendar.html'/>
                 <Frame title = 'React App' src = 'https://marclloyd01.github.io/build/index.html'/>
                 <Frame title = 'Portfolio with React' src = 'react-portfolio'/>
             </div>
